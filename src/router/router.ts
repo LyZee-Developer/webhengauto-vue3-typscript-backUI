66cd531c5c1548c7c7e6acd6641cee9e351c409e
leangsengLy <edeepleang@gmail.com>
2025-12-29 14:03:13 +0700

    back route

--- src/router/router.ts
+++ src/router/router.ts
@@ -1,9 +1,9 @@
 import { createRouter, createWebHistory, type RouteRecordRaw } from 'vue-router';
-// import NotFound from '../views/NotFound/NotFound.vue';
+import NotFound from '../views/NotFound/NotFound.vue';
 import Login from '../views/Login/Login.vue';
 import HomePage from '../views/Home.vue';
 import DesktopOrMobile from '../views/DesktopOrMobile.vue';
-// import { menu_data } from '../utils/system_data';
+import { menu_data } from '../utils/system_data';
 
 const routes: RouteRecordRaw[] = [
   {
@@ -16,33 +16,23 @@ const routes: RouteRecordRaw[] = [
     name: 'Home',
     component: HomePage,
   },
-  {
-    path: `/setting`,
-    name: `menu-setting`,
+  ...menu_data.map(val => ({
+    path: `/${val.code}`,
+    name: `menu-${val.code}`,
     component: DesktopOrMobile,
-  },
+  })),
+  ...menu_data.flatMap(menu =>
+    menu.subMenu.map(sub => ({
+      path: `/${menu.code}/${sub.code}`,
+      name: `menu-${menu.code}-${sub.code}`,
+      component: DesktopOrMobile,
+    }))
+  ),
   {
-    path: `/setting/owner`,
-    name: `menu-owner`,
-    component: DesktopOrMobile,
+    path: '/:pathMatch(.*)*',
+    name: 'NotFound',
+    component: NotFound,
   },
-  // ...menu_data.map(val => ({
-  //   path: `/${val.code}`,
-  //   name: `menu-${val.code}`,
-  //   component: DesktopOrMobile,
-  // })),
-  // ...menu_data.flatMap(menu =>
-  //   menu.subMenu.map(sub => ({
-  //     path: `/${menu.code}/${sub.code}`,
-  //     name: `menu-${menu.code}-${sub.code}`,
-  //     component: DesktopOrMobile,
-  //   }))
-  // ),
-  // {
-  //   path: '/:pathMatch(.*)*',
-  //   name: 'NotFound',
-  //   component: NotFound,
-  // },
 ];
 
 const router = createRouter({
